8b8f9b54438bce9122f26cdbe92d202f2e3e581f
Amir Reza Dalir <dalirnet@gmail.com>
2022-01-20 04:10:59 +0330

    config rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,23 @@
+import { terser } from "rollup-plugin-terser"
+
+const isProduction = process.env.NODE_ENV === "production"
+
+export default [
+    {
+        input: "src/index.mjs",
+        external: ["fs", "url", "path", "chalk", "lodash", "sharp", "mustache", "tesseract.js"],
+        plugins: isProduction ? [terser()] : [],
+        output: [
+            {
+                file: "dist/index.mjs",
+                format: "es",
+                compact: isProduction,
+            },
+            {
+                file: "dist/index.cjs",
+                format: "cjs",
+                compact: isProduction,
+            },
+        ],
+    },
+]
